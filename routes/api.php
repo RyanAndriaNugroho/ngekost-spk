c73fd5f226e8ece8d86de036c7ca7745765c1d79
RyanAndriaNugroho <ryan.nugroho@students.amikom.ac.id>
2020-07-18 20:55:30 +0700

    up hero

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
